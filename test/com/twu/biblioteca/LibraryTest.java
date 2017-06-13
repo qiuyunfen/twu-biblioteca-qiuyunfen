a0991eef92dade5798b04cbb8637650b6b4b35ed
qiuyunfen <1150382430@qq.com>
2017-06-13 10:40:06 +0800

    add print welcome message test

---
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -0,0 +1,24 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+    BibliotecaLib library;
+
+    @Before
+    public void setUp() {
+       library = new BibliotecaLib();
+    }
+    @Test
+    public void should_return_user_welcome_message() {
+        library.init("Lucy", "customer");
+        assertEquals(library.printWelcomeMsg(), "Lucy,Welcome to Biblioteca Library");
+    }
+    @After
+    public void tearDown() {
+        library = null;
+    }
+}
