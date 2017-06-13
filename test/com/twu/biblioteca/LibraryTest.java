3e1eb34e7a41fc5ff40321df35670efa73879d4d
qiuyunfen <1150382430@qq.com>
2017-06-13 17:09:03 +0800

    modify the sequence of test

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -21,37 +21,37 @@ public class LibraryTest {
     }
     @Test
     public void should_return_user_welcome_message() {
-        assertEquals(library.printWelcomeMsg(), "Lucy,Welcome to Biblioteca Library\n");
+        assertEquals("Lucy,Welcome to Biblioteca Library\n", library.printWelcomeMsg());
     }
 
     @Test
     public void should_return_uncheck_out_books_list() {
-        assertEquals(library.getUnCheckOutBooksList(), "List Books:\n2:bookName:Head First Java,author:Sierra k,year:2007\n");
+        assertEquals("List Books:\n2:bookName:Head First Java,author:Sierra k,year:2007\n", library.getUnCheckOutBooksList());
     }
 
     @Test
     public void should_return_checkout_book_list() {
-        assertEquals(library.getCheckoutBooksList(), "The books you have checked out:\n1:bookName:Test-driven Development: By Example,author:Kent Beck,year:2003\n");
+        assertEquals("The books you have checked out:\n1:bookName:Test-driven Development: By Example,author:Kent Beck,year:2003\n",library.getCheckoutBooksList());
     }
 
     @Test
     public void should_return_checkout_success() {
-        assertEquals(library.checkoutBook(2), "Thank you! Enjoy the book\n");
+        assertEquals("Thank you! Enjoy the book\n",library.checkoutBook(2));
     }
 
     @Test
     public void should_return_checkout_fail() {
-        assertEquals(library.checkoutBook(3), "That book is not available.\n");
+        assertEquals("That book is not available.\n",library.checkoutBook(3));
     }
 
     @Test
     public void should_return_return_book_success() {
-        assertEquals(library.returnBook(1), "Thank you for returning the book.\n");
+        assertEquals("Thank you for returning the book.\n",library.returnBook(1));
     }
 
     @Test
     public void should_return_return_book_fail() {
-        assertEquals(library.returnBook(3), "That is not a valid book to return.\n");
+        assertEquals("That is not a valid book to return.\n", library.returnBook(3));
     }
 
     @After
