8916ecc519d879d2eea219a3cbc19afbf158df99
qiuyunfen <1150382430@qq.com>
2017-06-15 15:47:22 +0800

    modify library

--- src/com/twu/biblioteca/model/LibraryThing.java
+++ src/com/twu/biblioteca/model/LibraryThing.java
@@ -1,10 +1,34 @@
 package com.twu.biblioteca.model;
 
-public interface LibraryThing {
-    public int id = 0;
-    public String name = "";
-    public int year = 0;
+import java.util.ArrayList;
 
-    public String checkOut();
-    public String returnThing();
+public class LibraryThing {
+    public int id;
+    public String name;
+    public int year;
+    public ArrayList<String> checkOutUser;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
